89a9e932d9f20ed631ac7d06563e48eadcce45db
unknown <giulia_pereira@saojose.sc.senai.br>
2019-06-17 08:44:56 -0300

    modulo 3 aula 2

---
+++ mod 3/exercicios/1/todo.js
@@ -0,0 +1,7 @@
+var listElement = document.querySelector("#app ul");
+var listInput = document.querySelector("#app imput");
+var listButton = document.querySelector("#app button");
+
+console.log(listelement);
+console.log(listInput);
+console.log(listButton);
